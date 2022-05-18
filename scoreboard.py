1da53862a4be479f026818ba59dfcb3fa491338b
Ajay Sajikumar <71449787+ajaysajikumar8@users.noreply.github.com>
2022-05-18 13:16:27 +0530

    update scoreboard.py

--- scoreboard.py
+++ scoreboard.py
@@ -1,7 +1,13 @@
 from turtle import Turtle
 
-
 class ScoreBoard(Turtle):
     def __init__(self):
         super().__init__()
-        self.write("Scoreboard: ")
+        self.hideturtle()
+        self.penup()
+        self.goto(0,285)
+        self.pendown()
+        self.color("white")
+        self.write("Scoreboard: ", align="center")
+        
+        
